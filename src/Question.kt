5d341716208fbf715fc99903a8c09332d6bbf986
Glujkin <kartoheckajaren@gmail.com>
2026-01-29 17:21:44 +0300

    Use an enum class

--- src/Question.kt
+++ src/Question.kt
@@ -1,5 +1,10 @@
 class Question<T>(
     val quetionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty,
 )
+enum class Difficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}
