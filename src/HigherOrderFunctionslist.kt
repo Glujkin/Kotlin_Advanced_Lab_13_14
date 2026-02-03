cdd3a617cbc390ab11a237659acf10ac5f56405b
Glujkin <kartoheckajaren@gmail.com>
2026-02-03 18:36:26 +0300

    map() function

--- src/HigherOrderFunctionslist.kt
+++ src/HigherOrderFunctionslist.kt
@@ -49,7 +49,11 @@ val cookies = listOf(
     )
 )
 fun main() {
-    cookies.forEach {
-        println("Пункт меню ${it.name}")
+    val fullmenu = cookies.map{
+        "${it.name} - $${it.price}"
+    }
+    println("Полное меню: ")
+    fullmenu.forEach {
+        println(it)
     }
 }
